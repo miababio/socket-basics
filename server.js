0d652f5afa31fd69422b0dc530eaffbbd660430c
Michael Ababio <miababio@gmail.com>
2017-10-19 02:08:53 -0500

    Added support for chat rooms; Modified Notes

--- server.js
+++ server.js
@@ -7,13 +7,26 @@ var moment = require("moment");
 
 app.use(express.static(__dirname + "/public"));
 
+var clientInfo = {};
+
 io.on("connection", function(socket) { // on -> lets you listen for events (name of event, function)
     console.log("User connected via socket.io!"); // prints when we get a connection
+    
+    socket.on("joinRoom", function(req) { // req -> contains name, room object we made in app.js
+        clientInfo[socket.id] = req;
+        socket.join(req.room); // join = built-in Socket method that tells the incoming socket to join a specific room
+        // broadcast.to(<rm_name>) -> send something to everyone in a specified room
+        socket.broadcast.to(req.room).emit("message", { 
+            name: "System",
+            text: req.name + " has joined!",
+            timestamp: moment().valueOf()
+        });
+    });
     
     socket.on("message", function(message) { // Listens for a message event
         console.log("Message received: " + message.text);
         message.timestamp = moment().valueOf();
-        io.emit("message", message); // Sends message to every other browser connected including us
+        io.to(clientInfo[socket.id].room).emit("message", message); // Sends message to every other browser connected including us that are in the same room as us
     });
     
     socket.emit("message", { // Emits an event; (event_name, data to send)
